897fb20ef07c42affa2f6b4e304aa1e679c27ac7
patrick.hage <patrick.h.hage@gmail.com>
2024-10-21 09:07:02 +0200

    add Record testing

--- src/test/java14/RecordTest.java
+++ src/test/java14/RecordTest.java
@@ -3,25 +3,43 @@ package java14;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
 import java.util.Set;
 
 public class RecordTest {
 
-    record Employee(String name, int age, String profession, int salary) {
+    record Employee(String name, int age, Profession profession, int salary) {
     }
+
+    class Profession {
+        private String title;
+        private String diploma;
+
+        public Profession(String title, String diploma) {
+            this.title = title;
+            this.diploma = diploma;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDiploma() {
+            return diploma;
+        }
+    }
+
 
     @Test
     void should_create_records () {
 
-        Set<Employee> employees = Set.of(
-                new Employee("John", 54, "Engineer", 60000),
-                new Employee("Lucas", 34, "Data analyst", 53000),
-                new Employee("Rebecca", 38, "Finance analyst", 55000)
-        );
+        Employee john = new Employee("John", 54, new Profession("Engineer", "Master"), 60000);
+        Employee lucas = new Employee("Lucas", 34, new Profession("Data analyst", "Master"), 53000);
+        Employee lisa = new Employee("Lisa", 38, new Profession("Finance analyst", "Master"), 55000);
+        Set<Employee> employees = Set.of(john, lucas, lisa);
 
         Assertions.assertEquals("Data analyst", employees.stream()
                 .filter(employee -> "Lucas".equals(employee.name))
-                .map(Employee::name));
+                .map(employee -> employee.profession.getTitle())
+                .findFirst().orElse("profession not found for employee"));
     }
 }
